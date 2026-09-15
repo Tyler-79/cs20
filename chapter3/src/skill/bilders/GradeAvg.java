b788c5b67306e883e35941ee247b963fe6f5f2d5
510204013 <510204013@L815WD220018.edu.cbe.ab.ca>
2026-09-15 12:04:32 -0600

    12:04 PM

--- chapter3/src/skill/bilders/GradeAvg.java
+++ chapter3/src/skill/bilders/GradeAvg.java
@@ -16,27 +16,48 @@ public class GradeAvg {
 		int g2t;
 		int g3t;
 		int g4t;
-		int g5t;
 		
 		
 		//Create Scanner object
 		Scanner userinput = new Scanner(System.in);
 		
-		//Get the 5 grades
+		//Get the 5 grades and after the grade is put in a average is shown
 		System.out.print("Enter the first grade: ");
 		G1 = userinput.nextInt();
 		
-		////g1t = G1;
+		g1t = G1 / 1;
+		
+		System.out.println("   your grade average is:  " + g1t);
+		
+		///                                                     \\\
 		
 		System.out.print("Enter the second grade: ");
 		G2 = userinput.nextInt();
 		
+		g2t = (G1 + G2) / 2 ;
+		
+		System.out.println("   your grade average is:  " + g2t);
+		
+		///                                                     \\\
+		
 		System.out.print("Enter the third grade: ");
 		G3 = userinput.nextInt();
+		
+		g3t = (G1 + G2 + G3) / 3 ;
+		
+		System.out.println("   your grade average is:  " + g3t);
+		
+		///                                                   \\\
 		
 		System.out.print("Enter the forth grade: ");
 		G4 = userinput.nextInt();
 		
+		g4t = (G1 + G2 + G3 + G4) / 4 ;
+		
+		System.out.println("   your grade average is:  " + g4t);
+		
+		///                                                    \\\
+				
 		System.out.print("Enter the last grade: ");
 		G5 = userinput.nextInt();
 		
@@ -44,7 +65,7 @@ public class GradeAvg {
 		Avg = (G1 + G2 + G3 + G4 + G5) / 5;
 		
 		//Display the grade average
-		System.out.println("your grade average is:  " + Avg);
+		System.out.println("  your grade average is:  " + Avg);
 
 	}
 
