6c18a8026f13b33cdd9e807c0003e6bb0e4a40e4
510204013 <510204013@L815WD220018.edu.cbe.ab.ca>
2026-09-22 11:36:08 -0600

    11:35 AM

--- chapter4/src/skill/bilders/PerfectSquare.java
+++ chapter4/src/skill/bilders/PerfectSquare.java
@@ -1,10 +1,20 @@
 package skill.bilders;
 
+import java.util.Scanner;
+
+import java.lang.Math;
+
 public class PerfectSquare {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static boolean main(int num1) {
+				if (num1 < 0) return false;
+				
+				int root = (int) Math.sqrt(num1);
+				return root * root == num1;
+				
+				
+						
+				
 	}
 
 }
