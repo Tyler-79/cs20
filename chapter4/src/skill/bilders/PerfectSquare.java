e3acb60752d295b4fd4f85b7c60a31a2297a49d0
510204013 <510204013@L815WD220018.edu.cbe.ab.ca>
2026-09-23 09:40:31 -0600

    9:40 AM

--- chapter4/src/skill/bilders/PerfectSquare.java
+++ chapter4/src/skill/bilders/PerfectSquare.java
@@ -7,20 +7,38 @@ import java.lang.Math;
 public class PerfectSquare {
 
 	public static void main(String[] args) {
-		//declare the integer
-				int num1 = 0;
-				
-				//scanner class for user
-				Scanner userinput = new Scanner(System.in);
-						
-				//prompt the user for integer
-				System.out.println("Enter the integer: ");
-				
-				//math part i think
-				
+		//declare the integers
+		double num, round, squrt, power;
+		
+		//scanner class for user
+		Scanner input = new Scanner(System.in);
+		
+		//prompt the user for a integer
+		System.out.println("Enter a integer: ");
+		
+		//store the integer given
+		num = input.nextInt();
+		
+		////testing
+		////System.out.println("the absolute of " + num + " is " + Math.abs(num));
+		////System.out.println(num + " to the power of 2 is " + Math.pow(num, 2));
+		////System.out.println("the square root of " + num + " is " + Math.sqrt(num));
+		
+		//math to find out if the given integer is a perfect square
+		squrt = Math.sqrt(num);
+		round = Math.round(squrt);
+		power = Math.pow(round, 2);
+		
+		////System.out.println("square root of " + num + " is " + squrt + " then rounding " + round + " to the power of 2 is " + power);
+		
+		//show user if the given integer is a perfect square or not
+		if (num != power) {System.out.println(num + " is not a perfect square root");}
+		else { System.out.println(num + " is a perfect square root");}
 				
 						
 				
 	}
+
+
 
 }
