b360809dea5d1f5186230af02e44a1e6c99054d9
510204013 <510204013@L815WD220018.edu.cbe.ab.ca>
2026-09-24 11:10:13 -0600

    11:10 AM

--- chapter4/src/mastry/GuessingGame.java
+++ chapter4/src/mastry/GuessingGame.java
@@ -1,9 +1,30 @@
 package mastry;
 
+import java.util.Scanner;
+
 public class GuessingGame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//declare both numbers
+		int player, secret;
+		
+		//scanner class for user
+		Scanner input = new Scanner(System.in);
+						
+		//prompt the user for a percentage
+		System.out.println("Enter a number between 1 and 20: ");
+						
+		//store the percentage given
+		player = input.nextInt();
+		
+		//computers number
+		secret = (int)(Math.random() * 21);  // 0 to 20 i think this should be the range
+		
+		//check is the players number is the same as the computers number and show both numbers
+		System.out.println("Computer's Number: " + secret);
+		System.out.println("Player's Number: " + player);
+		if (player == secret) {System.out.println("You won!");}
+		else {System.out.println("Better luck next time.");}
 
 	}
 
